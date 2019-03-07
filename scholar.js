16af801d0eaf7127fc669edd2b74615805585589
Stephen Makonin <stephen@makonin.com>
2019-03-06 22:59:58 -0800

    added paper stats

---
+++ scholar.js
@@ -0,0 +1,315 @@
+var sdata = {
+    agg: {
+        "Citations": 685,
+        "h-index": 13,
+        "i10-index": 18
+    },
+    papers: {
+        "cneuo_UAAAAJ:0EnyYjriUFMC": {
+            citations: 6,
+            id: "cneuo_UAAAAJ:0EnyYjriUFMC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Appliance Water Disaggregation via Non-Intrusive Load Monitoring (NILM)"
+        },
+        "cneuo_UAAAAJ:2osOgNQ5qMEC": {
+            citations: 54,
+            id: "cneuo_UAAAAJ:2osOgNQ5qMEC",
+            impact_factor: 3.022,
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "A Smarter Smart Home: Case Studies of Ambient Intelligence"
+        },
+        "cneuo_UAAAAJ:3fE2CSJIrl8C": {
+            citations: 0,
+            id: "cneuo_UAAAAJ:3fE2CSJIrl8C",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Chapter 18: App programming and its use in smart buildings"
+        },
+        "cneuo_UAAAAJ:7PzlFSSx8tAC": {
+            citations: 5,
+            id: "cneuo_UAAAAJ:7PzlFSSx8tAC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Incorporating Time-Of-Day Usage Patterns Into Non-Intrusive Load Monitoring"
+        },
+        "cneuo_UAAAAJ:9yKSN-GCB0IC": {
+            citations: 19,
+            id: "cneuo_UAAAAJ:9yKSN-GCB0IC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "The Affect of Lifestyle Factors on Eco-Visualization Design"
+        },
+        "cneuo_UAAAAJ:GnPB-g6toBAC": {
+            citations: 1,
+            id: "cneuo_UAAAAJ:GnPB-g6toBAC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "WaveNILM: A Causal Neural Network for Power Disaggregation from the Complex Power Signal"
+        },
+        "cneuo_UAAAAJ:LkGwnXOMwfcC": {
+            citations: 1,
+            id: "cneuo_UAAAAJ:LkGwnXOMwfcC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Transmitting Patient Vitals Over a Reliable ZigBee Mesh Network"
+        },
+        "cneuo_UAAAAJ:M3ejUd6NZC8C": {
+            citations: 0,
+            id: "cneuo_UAAAAJ:M3ejUd6NZC8C",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "CarbonKit: Designing A Personal Carbon Tracking Platform"
+        },
+        "cneuo_UAAAAJ:MXK_kJrjxJIC": {
+            citations: 0,
+            id: "cneuo_UAAAAJ:MXK_kJrjxJIC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "ODDs: Occupancy Detection Dataset"
+        },
+        "cneuo_UAAAAJ:NMxIlDl6LWMC": {
+            citations: 0,
+            id: "cneuo_UAAAAJ:NMxIlDl6LWMC",
+            impact_factor: 1.43,
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "HUE: The Hourly Usage of Energy Dataset for Buildings in British Columbia"
+        },
+        "cneuo_UAAAAJ:QIV2ME_5wuYC": {
+            citations: 0,
+            id: "cneuo_UAAAAJ:QIV2ME_5wuYC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Understanding Appliance Power Consumption Without Sensors"
+        },
+        "cneuo_UAAAAJ:Se3iqnhoufwC": {
+            citations: 4,
+            id: "cneuo_UAAAAJ:Se3iqnhoufwC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Cognitive Radio Technology: System Evolution"
+        },
+        "cneuo_UAAAAJ:Tyk-4Ss8FVUC": {
+            citations: 11,
+            id: "cneuo_UAAAAJ:Tyk-4Ss8FVUC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Inspiring energy conservation through open source power monitoring and in-home display"
+        },
+        "cneuo_UAAAAJ:ULOm3_A8WrAC": {
+            citations: 9,
+            id: "cneuo_UAAAAJ:ULOm3_A8WrAC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Investigating the Switch Continuity Principle Assumed in Non-Intrusive Load Monitoring (NILM)"
+        },
+        "cneuo_UAAAAJ:UebtZRa9Y70C": {
+            citations: 32,
+            id: "cneuo_UAAAAJ:UebtZRa9Y70C",
+            impact_factor: "",
+            is_book: true,
+            is_conference: false,
+            is_journal: false,
+            title: "Visual C++ 5.0 Developer's Guide"
+        },
+        "cneuo_UAAAAJ:W7OEmFMy1HYC": {
+            citations: 9,
+            id: "cneuo_UAAAAJ:W7OEmFMy1HYC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Inspiring energy conservation through open source metering hardware and embedded real-time load disaggregation"
+        },
+        "cneuo_UAAAAJ:WF5omc3nYNoC": {
+            citations: 18,
+            id: "cneuo_UAAAAJ:WF5omc3nYNoC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Real-time embedded low-frequency load disaggregation"
+        },
+        "cneuo_UAAAAJ:Y0pCki6q_DkC": {
+            citations: 22,
+            id: "cneuo_UAAAAJ:Y0pCki6q_DkC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "The cognitive power meter: Looking beyond the smart meter"
+        },
+        "cneuo_UAAAAJ:YFjsv_pBGBYC": {
+            citations: 0,
+            id: "cneuo_UAAAAJ:YFjsv_pBGBYC",
+            impact_factor: 2.45,
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "Residential Power Forecasting Using Load Identification and Graph Spectral Clustering"
+        },
+        "cneuo_UAAAAJ:YOwf2qJgpHMC": {
+            citations: 4,
+            id: "cneuo_UAAAAJ:YOwf2qJgpHMC",
+            impact_factor: "",
+            is_book: true,
+            is_conference: false,
+            is_journal: false,
+            title: "Start-Up Creation: The Smart Eco-efficient Built Environment"
+        },
+        "cneuo_UAAAAJ:YsMSGLbcyi4C": {
+            citations: 19,
+            id: "cneuo_UAAAAJ:YsMSGLbcyi4C",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Efficient Sparse Matrix Processing for Nonintrusive Load Monitoring (NILM)"
+        },
+        "cneuo_UAAAAJ:Zph67rFs4hoC": {
+            citations: 55,
+            id: "cneuo_UAAAAJ:Zph67rFs4hoC",
+            impact_factor: 4.836,
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "Electricity, water, and natural gas consumption of a residential house in Canada from 2012 to 2014"
+        },
+        "cneuo_UAAAAJ:_FxGoFyzp5QC": {
+            citations: 10,
+            id: "cneuo_UAAAAJ:_FxGoFyzp5QC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Approaches to Non-Intrusive Load Monitoring (NILM) in the Home"
+        },
+        "cneuo_UAAAAJ:_kc_bZDykSQC": {
+            citations: 16,
+            id: "cneuo_UAAAAJ:_kc_bZDykSQC",
+            impact_factor: 2.45,
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "Load Disaggregation Based on Aided Linear Integer Programming"
+        },
+        "cneuo_UAAAAJ:d1gkVwhDpl0C": {
+            citations: 11,
+            id: "cneuo_UAAAAJ:d1gkVwhDpl0C",
+            impact_factor: "tbd",
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "Home Occupancy Agent: Occupancy and Sleep Detection"
+        },
+        "cneuo_UAAAAJ:eQOLeE2rZwMC": {
+            citations: 13,
+            id: "cneuo_UAAAAJ:eQOLeE2rZwMC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "A consumer bill of rights for energy conservation"
+        },
+        "cneuo_UAAAAJ:hMod-77fHWUC": {
+            citations: 1,
+            id: "cneuo_UAAAAJ:hMod-77fHWUC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "A Recurrent Neural Network for Multisensory Non-Intrusive Load Monitoring on a Raspberry Pi"
+        },
+        "cneuo_UAAAAJ:hqOjcs7Dif8C": {
+            citations: 10,
+            id: "cneuo_UAAAAJ:hqOjcs7Dif8C",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Mixed-Initiative for Big Data: The Intersection of Human + Visual Analytics + Prediction"
+        },
+        "cneuo_UAAAAJ:kNdYIx-mwKoC": {
+            citations: 84,
+            id: "cneuo_UAAAAJ:kNdYIx-mwKoC",
+            impact_factor: 7.364,
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "Exploiting HMM Sparsity to Perform Online Real-Time Nonintrusive Load Monitoring"
+        },
+        "cneuo_UAAAAJ:mVmsd5A6BfQC": {
+            citations: 8,
+            id: "cneuo_UAAAAJ:mVmsd5A6BfQC",
+            impact_factor: "tbd",
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "RAE: The Rainforest Automation Energy Dataset for Smart Grid Meter Data Analysis"
+        },
+        "cneuo_UAAAAJ:u-x6o8ySG0sC": {
+            citations: 18,
+            id: "cneuo_UAAAAJ:u-x6o8ySG0sC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "Elements of consumption: an abstract visualization of household consumption"
+        },
+        "cneuo_UAAAAJ:u5HHmVD_uO8C": {
+            citations: 12,
+            id: "cneuo_UAAAAJ:u5HHmVD_uO8C",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "An intelligent agent for determining home occupancy using power monitors and light sensors"
+        },
+        "cneuo_UAAAAJ:ufrVoPGSRksC": {
+            citations: 65,
+            id: "cneuo_UAAAAJ:ufrVoPGSRksC",
+            impact_factor: 1.634,
+            is_book: false,
+            is_conference: false,
+            is_journal: true,
+            title: "Nonintrusive load monitoring (NILM) performance evaluation"
+        },
+        "cneuo_UAAAAJ:zYLM7Y9cAGgC": {
+            citations: 168,
+            id: "cneuo_UAAAAJ:zYLM7Y9cAGgC",
+            impact_factor: "",
+            is_book: false,
+            is_conference: true,
+            is_journal: false,
+            title: "AMPds: A public dataset for load disaggregation and eco-feedback research"
+        }
+    }
+}
