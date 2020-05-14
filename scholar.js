9512cd71a42e5e3ef52a422a5dae388c6b1efb0d
Stephen Makonin <stephen@makonin.com>
2020-05-14 08:41:30 -0700

    upd citation json

--- scholar.js
+++ scholar.js
@@ -1,6 +1,6 @@
 var sdata = {
     agg: {
-        "Citations": 1028,
+        "Citations": 1030,
         "h-index": 15,
         "i10-index": 23
     },
@@ -204,7 +204,7 @@ var sdata = {
             title: "Efficient Sparse Matrix Processing for Nonintrusive Load Monitoring (NILM)"
         },
         "cneuo_UAAAAJ:Zph67rFs4hoC": {
-            citations: 95,
+            citations: 96,
             id: "cneuo_UAAAAJ:Zph67rFs4hoC",
             impact_factor: 6.776,
             is_book: false,
@@ -267,7 +267,7 @@ var sdata = {
             title: "Mixed-Initiative for Big Data: The Intersection of Human + Visual Analytics + Prediction"
         },
         "cneuo_UAAAAJ:kNdYIx-mwKoC": {
-            citations: 156,
+            citations: 157,
             id: "cneuo_UAAAAJ:kNdYIx-mwKoC",
             impact_factor: 10.486,
             is_book: false,
